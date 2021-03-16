44a6eaa03ab3db19d640d7e4d9f9ef75c05ef0d7
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-17 09:57:32 +1100

    Renders book totiles to the DOM

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+.then(function(response) {
+  console.log(response);
+  return response.json();
+})
+.then(function(json) {
+  renderBooks(json);
+});
 }
 
 function renderBooks(books) {
+  console.log(books);
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
