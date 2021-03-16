09ac990f0d0cbcaf425ef4920d39dee92925eaf2
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-17 10:02:55 +1100

    Convert functions to arrow functions

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   fetch('https://anapioficeandfire.com/api/books')
-.then(function(response) {
-  console.log(response);
-  return response.json();
-})
-.then(function(json) {
-  renderBooks(json);
-});
+.then(response => response.json())
+.then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
-  console.log(books);
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
